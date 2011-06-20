7fa474234b9ea17ee54d6b917ee94112f87bf2be
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2011-06-20 10:49:51 +0200

    Include an I2C driver.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ PORT = /dev/ttyUSB0
 ARDUINO = /usr/share/arduino-0017/hardware/cores/arduino
 TARGET = pilot
 SRC = $(ARDUINO)/pins_arduino.c $(TARGET).c uart.c adc.c timer1.c actuators.c \
-      rx.c
+      rx.c twi.c
 MCU = atmega328p
 F_CPU = 16000000
 FORMAT = ihex
---
+++ twi.h
@@ -0,0 +1,21 @@
+/*
+ * TWI/I2C library for Wiring & Arduino
+ */
+
+#ifndef TWI_FREQ
+# define TWI_FREQ 100000L
+#endif
+
+#ifndef TWI_BUFFER_LENGTH
+# define TWI_BUFFER_LENGTH 8
+#endif
+
+#define TWI_READY	0
+#define TWI_MRX		1
+#define TWI_MTX		2
+#define TWI_SRX		3
+#define TWI_STX		4
+
+void twi_init(void);
+void i2c_send_byte(uint8_t address, uint8_t byte);
+void i2c_request_bytes(uint8_t address, uint8_t count, uint8_t *out);
